d046e475b3b076c39a24f6a353bfcec19d057b53
LLFRealms <llfrealms@gmail.com>
2014-03-12 16:02:44 -0400

    Initial Commit

---
+++ src/com/llfrealms/OnTimeRankUp/OnTimeRankUp.java
@@ -0,0 +1,41 @@
+package com.llfrealms.OnTimeRankUp;
+
+import net.milkbowl.vault.economy.Economy;
+import net.milkbowl.vault.permission.Permission;
+
+import org.bukkit.plugin.RegisteredServiceProvider;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class OnTimeRankUp extends JavaPlugin
+{
+	public static Economy economy = null;
+	public static Permission permission = null;
+	
+	public void onEnable()
+	{
+		setupEconomy();
+		setupPermissions();
+		
+	}
+	public void onDisable()
+	{
+		
+	}
+	private boolean setupEconomy()
+    {
+        RegisteredServiceProvider<Economy> economyProvider = getServer().getServicesManager().getRegistration(net.milkbowl.vault.economy.Economy.class);
+        if (economyProvider != null) {
+            economy = economyProvider.getProvider();
+        }
+
+        return (economy != null);
+    }
+	private boolean setupPermissions()
+    {
+        RegisteredServiceProvider<Permission> permissionProvider = getServer().getServicesManager().getRegistration(net.milkbowl.vault.permission.Permission.class);
+        if (permissionProvider != null) {
+            permission = permissionProvider.getProvider();
+        }
+        return (permission != null);
+    }
+}
